2a31c56b9d5d20c7c90f6ccaa89ced104cdb9f66
CodingManoj <mvp2612@hotmail.com>
2023-02-15 19:59:12 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/b52-clouddevops/tf-module-vpc.git?$ABC"
+  source = "git::https://github.com/b52-clouddevops/tf-module-vpc.git"
 }
 
 # By default or this only downloads the code from the main branch of the repository
