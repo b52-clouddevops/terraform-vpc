999620dfcc067eea9e8fe1fd7ca55bfb0ecbd077
CodingManoj <mvp2612@hotmail.com>
2023-02-15 20:20:00 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@
 
 
 module "vpc" {
-  source = "./vendor/modules/vpc"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  source        = "./vendor/modules/vpc"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
 }
 
-VPC_CIDR
