2b3bd4793a4e338fe602da88959a416f8dabaea4
CodingManoj <mvp2612@hotmail.com>
2023-02-15 20:19:08 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
-module "vpc" {
-  source = "git::https://github.com/b52-clouddevops/tf-module-vpc.git"
-}
+# module "vpc" {
+#   source = "git::https://github.com/b52-clouddevops/tf-module-vpc.git"
+# }
 
 # By default or this only downloads the code from the main branch of the repository
 
 # On module sources, we cannot parameterize the value of the source.
 
 # How can we tell a module source to close it from a specific branch ????
+
+
+
+# module "vpc" {
+#   source = "git::https://github.com/b52-clouddevops/tf-module-vpc.git"
+# }
 
 VPC_CIDR
