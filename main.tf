a7cbea91cf0b42258aca3e6161749d6010cbf606
CodingManoj <mvp2612@hotmail.com>
2023-02-15 19:59:04 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 module "vpc" {
-  source = "git::https://github.com/b52-clouddevops/tf-module-vpc.git?ref=main"
+  source = "git::https://github.com/b52-clouddevops/tf-module-vpc.git?$ABC"
 }
 
 # By default or this only downloads the code from the main branch of the repository
+
+# On module sources, we cannot parameterize the value of the source.
