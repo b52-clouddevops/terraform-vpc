0f7539383054a61ea2c475edc19efe512261749c
CodingManoj <mvp2612@hotmail.com>
2023-02-15 20:15:40 -0500

    Auto Push Commit

--- provider.tf
+++ provider.tf
@@ -2,3 +2,11 @@ provider "aws" {
     region  = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "b52-terraform-state-bucket"
+    key    = "ec2/dev/terraform.tfstate"
+    region = "us-east-1"
+    }
+}
+
