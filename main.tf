b06cbebcc0ad857f8aa4f27228328b17ac141770
CodingManoj <mvp2612@hotmail.com>
2023-02-17 20:18:03 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ module "vpc" {
   AZ                   = var.AZ   
   PUBLIC_SUBNET_CIDR   = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR  = var.PRIVATE_SUBNET_CIDR
+  DEFAULT_VPC_CIDR     = var.DEFAULT_VPC_CIDR
 
 }
 
