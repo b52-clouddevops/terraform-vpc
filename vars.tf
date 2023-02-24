323b03dc4cad3584aa63cdb6352fda53f4d12c67
CodingManoj <mvp2612@hotmail.com>
2023-02-23 19:56:24 -0500

    Auto Push Commit

--- vars.tf
+++ vars.tf
@@ -5,12 +5,10 @@ variable "DEFAULT_VPC_RT" {}
 variable "PRIVATE_HOSTEDZONE_ID" {}
 variable "PUBLIC_HOSTEDZONE_ID" {}
 variable "PRIVATE_HOSTEDZONE_NAME" {}
-variable ""
+variable "PUBLIC_HOSTEDZONE_NAME" {}
 variable "ENV" {}
 variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
 variable "AZ" {}
 
 
-PRIVATE_HOSTEDZONE_NAME = "roboshop.internal"
-PUBLIC_HOSTEDZONE_NAME = "robos-shop.online"
